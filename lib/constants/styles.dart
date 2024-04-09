ce7f407e30a7ab2757caf3d70bbf92283b3e688f
Joel Sebastian Villarojo <sebastianvillarojo16@gmail.com>
2024-04-09 14:46:39 +0800

    added styles

---
+++ lib/constants/styles.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kWhite = Color(0xFFFFFFFF);
+
+final SoraBold = GoogleFonts.sora(
+  fontWeight: FontWeight.w700,
+);
